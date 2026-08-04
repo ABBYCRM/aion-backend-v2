52d6a54dd4c8c02aa0e90b3b9d9e94560467b7fa
Mavis <Mavis@abbycrm.com>
2026-08-04 01:53:42 +0000

    debug env

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from .settings import settings
-
+import os
 app = FastAPI()
 @app.get("/healthz")
 def h():
-    return {"ok": True, "service": settings.app_name, "version": settings.app_version}
+    return {"ok": True, "openrouter_key_len": len(os.environ.get("OPENROUTER_API_KEY", ""))}
+@app.get("/debug")
+def d():
+    return {k: (v[:30] + "...") if len(v) > 30 else v for k, v in os.environ.items() if k in ["OPENROUTER_API_KEY", "OPENROUTER_BASE_URL", "APP_NAME", "AUDIT_LOG_DIR"]}
